6686f83368428eda4dfb45f1b6e3cf59447ac42b
Nishant Yadav <nishantnirwanofficial@gmail.com>
2026-08-09 11:09:58 +0530

    Time: 21 ms (14.34%), Space: 44.2 MB (82.63%) - LeetHub

---
+++ 1140-stone-game-ii/1140-stone-game-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int stoneGameII(int[] piles) {
+        int n = piles.length;
+
+        int[] suffix = new int[n + 1];
+
+        for (int i = n - 1; i >= 0; i--) {
+            suffix[i] = suffix[i + 1] + piles[i];
+        }
+
+        int[][] dp = new int[n + 1][n + 1];
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int m = n; m >= 1; m--) {
+                for (int x = 1; x <= 2 * m && i + x <= n; x++) {
+                    dp[i][m] = Math.max(
+                        dp[i][m],
+                        suffix[i] - dp[i + x][Math.max(m, x)]
+                    );
+                }
+            }
+        }
+
+        return dp[0][1];
+    }
+}
