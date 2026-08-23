fc4d4d49b9157d54d31366205749a6b06ab58015
Nishant Yadav <nishantnirwanofficial@gmail.com>
2026-08-23 13:24:43 +0530

    Time: 7 ms (79.56%), Space: 46.9 MB (83.94%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean sumGame(String num) {
+        int n = num.length();
+        int cnt1 = 0, cnt2 = 0;
+        int sum1 = 0, sum2 = 0;
+
+        for (int i = 0; i < n / 2; i++) {
+            if (num.charAt(i) == '?') {
+                cnt1++;
+            } else {
+                sum1 += num.charAt(i) - '0';
+            }
+        }
+
+        for (int i = n / 2; i < n; i++) {
+            if (num.charAt(i) == '?') {
+                cnt2++;
+            } else {
+                sum2 += num.charAt(i) - '0';
+            }
+        }
+
+        return (cnt1 + cnt2) % 2 == 1 ||
+               2 * (sum1 - sum2) != 9 * (cnt2 - cnt1);
+    }
+}
